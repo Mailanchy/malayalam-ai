e08bf008de849291d74f5cf1f38eca6d3ce51270
Abdul Haseeb <haseeb.a@2basetechnologies.com>
2025-04-06 15:59:49 +0530

    Cleaned design

--- lib/record_to_stream_example.dart
+++ lib/record_to_stream_example.dart
@@ -204,62 +204,26 @@ class _RecordToStreamExampleState extends State<RecordToStreamExample> {
   Widget build(BuildContext context) {
     Widget makeBody() {
       return Column(children: [
-        Container(
-          margin: const EdgeInsets.all(3),
-          padding: const EdgeInsets.all(3),
-          height: 120,
-          width: double.infinity,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: const Color(0xFFFAF0E6),
-            border: Border.all(
-              color: Colors.indigo,
-              width: 3,
-            ),
+        Column(children: [
+          ElevatedButton(
+            onPressed: getRecorderFn(),
+            //color: Colors.white,
+            //disabledColor: Colors.grey,
+            child: Text(_mRecorder!.isRecording ? 'Stop' : 'Record'),
           ),
-          child: Column(children: [
-            Row(children: [
-              ElevatedButton(
-                onPressed: getRecorderFn(),
-                //color: Colors.white,
-                //disabledColor: Colors.grey,
-                child: Text(_mRecorder!.isRecording ? 'Stop' : 'Record'),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Text(_mRecorder!.isRecording
-                  ? 'Recording in progress'
-                  : 'Recorder is stopped'),
-            ]),
-            const SizedBox(
-              height: 20,
-            ),
-            _mRecorder!.isRecording
-                ? LinearProgressIndicator(
-                    value: _dbLevel / 100,
-                    valueColor:
-                        const AlwaysStoppedAnimation<Color>(Colors.indigo),
-                    backgroundColor: Colors.limeAccent)
-                : Container(),
-          ]),
-        ),
-        IconButton(
-          onPressed: getPlaybackFn(),
-          //color: Colors.white,
-          //disabledColor: Colors.grey,
-          icon: _mPlayer!.isPlaying
-              ? const Icon(Icons.pause)
-              : const Icon(Icons.play_arrow),
-        ),
+          IconButton(
+            onPressed: getPlaybackFn(),
+            //color: Colors.white,
+            //disabledColor: Colors.grey,
+            icon: _mPlayer!.isPlaying
+                ? const Icon(Icons.pause)
+                : const Icon(Icons.play_arrow),
+          ),
+        ]),
       ]);
     }
 
     return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        title: const Text('Record to Stream ex.'),
-      ),
       body: makeBody(),
     );
   }
